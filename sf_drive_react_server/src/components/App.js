7c53b736228fe79e31dcd54f58935cff0e014695
Server <server@server.com>
2021-03-11 23:18:15 +0300

    docs: updated file sf_drive_react_server, added password part and validation fixed

---
+++ sf_drive_react_server/src/components/App.js
@@ -0,0 +1,35 @@
+import * as React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+import "../styles/App.css";
+
+import MainPage from "./MainPage";
+import About from "./About";
+import Faq from "./Faq";
+import Conditions from "./Conditions";
+import RegistrationPage from "./RegistrationPage/RegistrationPage.js";
+
+function App() {
+    
+    return (
+      <Router>
+            <Switch>
+             <Route exact path="/" component={RegistrationPage} />
+             <Route exact path="/MainPage" component={MainPage} /> 
+             <Route exact path="/About" component={About} /> 
+             <Route exact path="/Faq" component={Faq} />
+             <Route exact path="/conditions" component={Conditions} />
+           </Switch>
+      </Router>
+
+    ); 
+    
+}
+
+export default App;
